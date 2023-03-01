fe23720b1bfd16e1acc9d62b7749d2743f967901
Dianav21 <dianavargas214@gmail.com>
2023-03-01 18:32:23 -0500

    added the author routes

---
+++ server/routes/author.js
@@ -0,0 +1,44 @@
+import express from "express"
+import {prisma} from "../db/index.js"
+
+export default function authorRouter(passport){
+    const router = express.Router();
+
+router.post("/",passport.authenticate("jwt", {session: false}), async (req, res) => {
+    //creates / adds an author
+    
+    const newAuthor = await prisma.author.create({
+        data: {
+            name: req.body.author,
+            // description: req.body.description,
+            userId: req.user.id,
+        }
+    });
+    console.log(newAuthor);
+
+    res.status(201).json({
+        success: true
+    });
+});
+
+//updates author
+router.put("/:authorId",passport.authenticate("jwt", {session: false}), async (req, res) => {
+    const updateAuthor = await prisma.author.update({
+        where: {
+            id: parseInt(req.params.authorId)
+        },
+        data: {
+            name: req.body.author,
+            // description: req.body.description
+            userId: req.user.id,
+        }
+    });
+    console.log(updateAuthor);
+    //sends back response if it works 
+    res.status(200).json({
+        success: true, 
+        message: "author name has been edited."
+    });
+})
+return router;
+}
