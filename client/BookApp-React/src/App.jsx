6b317ad754889226c9bda4f2f4629386cc462e5f
ChineduEjiogu1 <chineduejiogu@gmail.com>
2023-03-01 21:11:50 -0500

    Feature: login frontend style

--- client/BookApp-React/src/App.jsx
+++ client/BookApp-React/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import LoginPage from '../Pages/LoginPage'
 import './App.css'
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
 
   return (
     <div className="App">
-
+      <LoginPage/>
     </div>
   )
 }
