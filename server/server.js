360cff00105bf984f7d7fd1a5f7a12900e693557
Luis Molinuevo <luismolinuevo0@gmail.com>
2023-03-01 19:38:55 -0500

    add author middle to server

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import authRouter from "./routes/auth.js";
 import bookRouter from "./routes/books.js";
 import setupJWTStrategy from "./middlewares/auth.js";
 import passport from "passport";
+import authorRouter from "./routes/author.js";
 
 export default function createServer() {
     const app = express();
@@ -16,6 +17,7 @@ export default function createServer() {
 
     app.use("/auth", authRouter);
     app.use("/books", bookRouter(passport));
+    app.use("/author", authorRouter(passport));
 
     return app;
 }
