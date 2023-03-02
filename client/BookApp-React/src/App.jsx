e292ec95e677a91813f9f77e5c53c77ce6bc76e9
Luis Molinuevo <luismolinuevo0@gmail.com>
2023-03-01 21:21:41 -0500

    set up react router

--- client/BookApp-React/src/App.jsx
+++ client/BookApp-React/src/App.jsx
@@ -2,12 +2,33 @@ import { useState } from 'react'
 import LoginPage from '../Pages/LoginPage'
 import './App.css'
 
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+  Outlet,
+  createRoutesFromElements,
+} from "react-router-dom";
+import './App.css'
+
 function App() {
 
+  const router = createBrowserRouter([
+    {
+      path: "/"
+    },
+    {
+      path:"/login",
+      element: <LoginPage/>
+    }
+
+  ])
 
   return (
     <div className="App">
-      <LoginPage/>
+      <RouterProvider router={router}/>
     </div>
   )
 }
