35a5e4f1e8fbb41ab322343f540263f90d85feda
Qojedi57 <111308861+Qojedi57@users.noreply.github.com>
2023-03-06 15:23:27 -0800

    Added the map method in front-end

--- client/BookApp-React/Pages/DisplayBooks.jsx
+++ client/BookApp-React/Pages/DisplayBooks.jsx
@@ -1,32 +1,36 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 // import fetchBooks from './GetAllBooks'
 import axios from "axios";
 
 export default function DisplayBooks() {
-    const [books, setBooks] = useState([]);
-    const [error, setError] = useState(false);
-    useEffect(() => {
-        const fetchBooks = async () => {
-            try{
-                const allBooks = await axios.get("http://localhost:8080/books");
-                console.log(allBooks);
-                if(allBooks.status === 200){
-                    setBooks(allBooks.data)
-                } else {
-                  return null;
-                }
-              } catch(error){
-              console.log("Oh no, something went wrong", error);
-            }
+  const [books, setBooks] = useState([]);
+  const [error, setError] = useState(false);
+  useEffect(() => {
+    const fetchBooks = async () => {
+      try {
+        const allBooks = await axios.get("http://localhost:8080/books");
+        console.log(allBooks);
+        if (allBooks.status === 200) {
+          setBooks(allBooks.data);
+        } else {
+          return null;
         }
+      } catch (error) {
+        console.log("Oh no, something went wrong", error);
+      }
+    };
 
-        fetchBooks();
-    },[]) 
+    fetchBooks();
+  }, []);
 
-    //Eduardo map throught the books state. And display the data. Could do it here or make a new component that takes in props
+  //Eduardo map throught the books state. And display the data. Could do it here or make a new component that takes in props
   return (
-    <div>
-
+    <div className="Novels">
+      {books.map((item) => (
+        <div className="Novels">
+            <h1>{item.title}</h1>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
