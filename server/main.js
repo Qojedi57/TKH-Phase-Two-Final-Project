1f508fa89e4514342982339ac8e9b85a5725d3ee
Qojedi57 <111308861+Qojedi57@users.noreply.github.com>
2023-06-30 15:01:10 -0700

    Chore:Updated the Url for the server for the backend added the deployed Url

--- server/main.js
+++ server/main.js
@@ -3,5 +3,6 @@ import createServer from "./server.js";
 const server = createServer();
 const PORT = process.env.PORT  || 8080;
 server.listen(PORT, () => {
-  console.log("Server is listening at localhost:8080");
+  console.log("Server is listening at https://tkh-phase-two-final-project-production.up.railway.app ");
+  // localhost:8080
 });
